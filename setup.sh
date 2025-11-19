26eebea455746996dbd1bcd91667c708e690ae04
bhickta <61617746+bhickta@users.noreply.github.com>
2025-11-19 20:00:09 +0530

    chmod full developer

--- setup.sh
+++ setup.sh
@@ -3,5 +3,10 @@ echo "Installing Ansible..."
 sudo apt-get update
 sudo apt-get install -y ansible git
 
+# 2. Fix Home Permissions (Allows 'frappe' user to read Ansible tmp files)
+echo "Relaxing Home Directory Permissions..."
+# Grants Read/Execute access to everyone for the current user's home
+sudo chmod o+rx $HOME
+
 echo "Running Playbook..."
 ansible-playbook -i inventory/hosts site.yml
